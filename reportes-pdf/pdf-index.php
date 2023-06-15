3c4b3aa1a254440102d8abbbc1e8df645957a225
yordysh <yordysh28@gmail.com>
2023-06-15 17:29:24 -0500

    Add pdf

--- reportes-pdf/pdf-index.php
+++ reportes-pdf/pdf-index.php
@@ -88,28 +88,12 @@ ob_start();
 </style>
 
 <table>
-    <!-- <thead>
-        <tr>
-            <th rowspan="4" style="border-right: none;">Columna 1</th>
-            <th rowspan="4" style="border-left: none;">Columna 2</th>
-            <th>Fila 1</th>
-        </tr>
-        <tr>
-            <th>Fila 2</th>
-        </tr>
-        <tr>
-            <th>Fila 3</th>
-        </tr>
-        <tr>
-            <th>Fila 4</th>
-        </tr>
-    </thead> -->
     <tbody>
         <tr>
             <td rowspan="2">Zonas/areas</td>
             <td rowspan="2">Infraestructura, accesorios complementarios</td>
-            <td colspan="4">Diario</td>
-            <td colspan="4">Interdiario</td>
+            <td colspan="24">Diario</td>
+            <td colspan="16">Interdiario</td>
             <td colspan="4">Semanal</td>
             <td colspan="4">Quincenal</td>
             <td colspan="4">Mensual</td>
@@ -138,91 +122,128 @@ ob_start();
             <td>S4</td>
         </tr>
         <tr>
+            <td rowspan="2">XXX</td>
+        </tr>
+        <tr>
             <td>xxx</td>
+        </tr>
+        <tr>
             <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <!-- <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td> -->
-            <!-- <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td>
-            <td>xxx</td> -->
         </tr>
 
-
         <!-- <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-            <td>10</td>
-            <td>11</td>
-            <td>12</td>
-            <td>13</td>
-            <td>14</td>
-            <td>15</td>
-            <td>16</td>
-            <td>17</td>
-            <td>18</td>
-            <td>19</td>
-            <td>20</td>
-            <td>21</td>
-            <td>22</td>
-            <td>23</td>
-            <td>24</td>
-            <td>25</td>
-            <td>26</td>
-            <td>27</td>
-            <td>28</td>
-            <td>29</td>
-            <td>30</td>
-            <td>31</td>
-            <td>32</td>
-            <td>33</td>
-            <td>34</td>
-            <td>35</td>
-            <td>36</td>
-            <td>37</td>
-            <td>38</td>
-            <td>40</td>
-            <td>41</td>
-            <td>42</td>
-            <td>43</td>
-            <td>44</td>
-            <td>45</td>
-            <td>46</td>
-            <td>47</td>
-            <td>48</td>
-            <td>49</td>
-            <td>50</td>
-            <td>51</td>
-            <td>52</td>
-            <td>53</td>
-            <td>54</td>
-            <td>55</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+        </tr> -->
+        <!-- <tr>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
+            <td>xxx</td>
         </tr> -->
     </tbody>
 </table>
@@ -241,7 +262,7 @@ $dompdf = new Dompdf();
 $dompdf->loadHtml($html);
 
 // Set the paper size and orientation
-$dompdf->setPaper('A3', 'landscape');
+$dompdf->setPaper('A2', 'landscape');
 
 // Render the HTML content to PDF
 $dompdf->render();
