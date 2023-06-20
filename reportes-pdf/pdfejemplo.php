c778f6c6e7d6abf10f57e3e4896d892a7bb0caff
yordysh <yordysh28@gmail.com>
2023-06-20 12:58:14 -0500

    Adding status in fecha_total

--- reportes-pdf/pdfejemplo.php
+++ reportes-pdf/pdfejemplo.php
@@ -18,124 +18,6 @@ $html .= '<table>';
 $html .= '<tbody>';
 
 
-// $grupos = array();
-
-// foreach ($datos as $fila) {
-//     $nombreZona = $fila['NOMBRE_T_ZONA_AREAS'];
-//     $nombreInfraestructura = $fila['NOMBRE_INFRAESTRUCTURA'];
-//     $estado = $fila['ESTADO'];
-//     $fechaTotal = $fila['FECHA_TOTAL'];
-
-//     if (!isset($grupos[$nombreZona])) {
-//         $grupos[$nombreZona] = array();
-//     }
-
-//     $grupos[$nombreZona][] = array(
-//         'nombreInfraestructura' => $nombreInfraestructura,
-//         'estado' => $estado,
-//         'fechaTotal' => $fechaTotal
-//     );
-// }
-
-// foreach ($grupos as $nombreZona => $valores) {
-//     $html .= '<tr>';
-//     $html .= '<td rowspan="' . count($valores) . '">' . $nombreZona . '</td>';
-//     $html .= '<td>' . $valores[0]['nombreInfraestructura'] . '</td>';
-//     $html .= '<td>' . $valores[0]['estado'] . '</td>';
-//     // $html .= '<td>' . ($valores[0]['fechaTotal'] !== null && $valores[0]['fechaTotal'] != "" ? $valores[0]['fechaTotal'] : '') . '</td>';
-//     $html .= '</tr>';
-
-//     for ($i = 1; $i < count($valores); $i++) {
-//         $html .= '<tr>';
-//         $html .= '<td>' . $valores[$i]['nombreInfraestructura'] . '</td>';
-//         $html .= '<td>' . $valores[$i]['estado'] . '</td>';
-//         // $html .= '<td>' . ($valores[$i]['fechaTotal'] !== null && $valores[$i]['fechaTotal'] != "" ? $valores[$i]['fechaTotal'] : '') . '</td>';
-//         $html .= '</tr>';
-//     }
-// }
-
-
-
-// $grupos = array();
-
-// foreach ($datos as $fila) {
-//     $nombreZona = $fila['NOMBRE_T_ZONA_AREAS'];
-//     $nombreInfraestructura = $fila['NOMBRE_INFRAESTRUCTURA'];
-//     $estado = $fila['ESTADO'];
-//     $fechaTotal = $fila['FECHA_TOTAL'];
-
-//     if (!isset($grupos[$nombreZona])) {
-//         $grupos[$nombreZona] = array();
-//     }
-
-//     $grupos[$nombreZona][] = array(
-//         'nombreInfraestructura' => $nombreInfraestructura,
-//         'estado' => $estado,
-//         'fechaTotal' => $fechaTotal
-//     );
-// }
-
-
-// foreach ($grupos as $nombreZona => $valores) {
-//     $html .= '<tr>';
-//     $html .= '<td rowspan="' . count($valores) . '">' . $nombreZona . '</td>';
-//     $html .= '<td>' . $valores[0]['nombreInfraestructura'] . '</td>';
-//     $html .= '<td>' . $valores[0]['estado'] . '</td>';
-
-//     // Obtener el mes y año de fechaTotal del primer valor
-//     $fechaTotal = new DateTime($valores[0]['fechaTotal']);
-//     $mes = $fechaTotal->format('m');
-//     $anio = $fechaTotal->format('Y');
-
-//     // Obtener el último día del mes
-//     $ultimoDiaMes = (new DateTime($anio . '-' . $mes . '-01'))->format('t');
-
-//     // Calcular el número total de columnas
-//     $numColumnas = 3 + $ultimoDiaMes;
-
-//     // Imprimir columnas adicionales a partir de la columna 4
-//     for ($i = 4; $i <= $numColumnas; $i++) {
-//         $html .= '<td>' . ($i - 3) . '</td>';
-//     }
-
-//     // Completar las columnas faltantes hasta 31 días
-//     for ($i = $numColumnas + 1; $i <= 34; $i++) {
-//         $html .= '<td></td>';
-//     }
-
-//     $html .= '</tr>';
-
-//     for ($j = 1; $j < count($valores); $j++) {
-//         $html .= '<tr>';
-//         $html .= '<td>' . $valores[$j]['nombreInfraestructura'] . '</td>';
-//         $html .= '<td>' . $valores[$j]['estado'] . '</td>';
-
-//         // Obtener el mes y año de fechaTotal del valor actual
-//         $fechaTotal = new DateTime($valores[$j]['fechaTotal']);
-//         $mes = $fechaTotal->format('m');
-//         $anio = $fechaTotal->format('Y');
-
-//         // Obtener el último día del mes
-//         $ultimoDiaMes = (new DateTime($anio . '-' . $mes . '-01'))->format('t');
-
-//         // Calcular el número total de columnas
-//         $numColumnas = 3 + $ultimoDiaMes;
-
-//         // Imprimir columnas adicionales a partir de la columna 4
-//         for ($k = 4; $k <= $numColumnas; $k++) {
-//             $html .= '<td>' . ($k - 3) . '</td>';
-//         }
-
-//         // Completar las columnas faltantes hasta 31 días
-//         for ($k = $numColumnas + 1; $k <= 34; $k++) {
-//             $html .= '<td></td>';
-//         }
-
-//         $html .= '</tr>';
-//     }
-// }
-
-
 
 $grupos = array();
 
@@ -163,27 +45,15 @@ foreach ($grupos as $nombreZona => $valores) {
     $html .= '<tr>';
     $html .= '<td rowspan="' . count($valores) . '">' . $nombreZona . '</td>';
     $html .= '<td>' . $valores[0]['nombreInfraestructura'] . '</td>';
-    $html .= '<td>' . $valores[0]['estado'] . '</td>';
 
     // Obtener el mes y año de fechaTotal del primer valor
     $fechaTotal = new DateTime($valores[0]['fechaTotal']);
     $mes = $fechaTotal->format('m');
     $anio = $fechaTotal->format('Y');
 
-    // Obtener el último día del mes
-    $ultimoDiaMes = (new DateTime($anio . '-' . $mes . '-01'))->format('t');
-
-    // Calcular el número total de columnas
-    $numColumnas = 3 + $ultimoDiaMes;
-
-    // Imprimir columnas adicionales a partir de la columna 4
-    for ($i = 4; $i <= $numColumnas; $i++) {
-        $html .= '<td>' . ($i - 3) . '</td>';
-    }
-
-    // Completar las columnas faltantes hasta 31 días
-    for ($i = $numColumnas + 1; $i <= 34; $i++) {
-        $html .= '<td></td>';
+    // Imprimir columnas para los días del mes
+    for ($dia = 1; $dia <= 31; $dia++) {
+        $html .= '<td>' . ($mes == $fechaTotal->format('m') && $anio == $fechaTotal->format('Y') && $dia == $fechaTotal->format('d') ? $valores[0]['estado'] : '') . '</td>';
     }
 
     $html .= '</tr>';
@@ -191,32 +61,26 @@ foreach ($grupos as $nombreZona => $valores) {
     for ($j = 1; $j < count($valores); $j++) {
         $html .= '<tr>';
         $html .= '<td>' . $valores[$j]['nombreInfraestructura'] . '</td>';
-        $html .= '<td>' . $valores[$j]['estado'] . '</td>';
 
         // Obtener el mes y año de fechaTotal del valor actual
         $fechaTotal = new DateTime($valores[$j]['fechaTotal']);
         $mes = $fechaTotal->format('m');
         $anio = $fechaTotal->format('Y');
 
-        // Obtener el último día del mes
-        $ultimoDiaMes = (new DateTime($anio . '-' . $mes . '-01'))->format('t');
-
-        // Calcular el número total de columnas
-        $numColumnas = 3 + $ultimoDiaMes;
-
-        // Imprimir columnas adicionales a partir de la columna 4
-        for ($k = 4; $k <= $numColumnas; $k++) {
-            $html .= '<td>' . ($k - 3) . '</td>';
-        }
-
-        // Completar las columnas faltantes hasta 31 días
-        for ($k = $numColumnas + 1; $k <= 34; $k++) {
-            $html .= '<td></td>';
+        // Imprimir columnas para los días del mes
+        for ($dia = 1; $dia <= 31; $dia++) {
+            $html .= '<td>' . ($mes == $fechaTotal->format('m') && $anio == $fechaTotal->format('Y') && $dia == $fechaTotal->format('d') ? $valores[$j]['estado'] : '') . '</td>';
         }
 
         $html .= '</tr>';
     }
 }
+
+
+
+
+
+
 
 
 
