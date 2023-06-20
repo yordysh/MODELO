0f838ad2d9b3a229a0495d08471a4e8f6daedd61
yordysh <yordysh28@gmail.com>
2023-06-20 12:27:28 -0500

    adding ESTADO !P show table PDF

--- reportes-pdf/pdfejemplo.php
+++ reportes-pdf/pdfejemplo.php
@@ -54,24 +54,27 @@ $html .= '<tbody>';
 //     }
 // }
 
-$grupos = array();
 
-foreach ($datos as $fila) {
-    $nombreZona = $fila['NOMBRE_T_ZONA_AREAS'];
-    $nombreInfraestructura = $fila['NOMBRE_INFRAESTRUCTURA'];
-    $estado = $fila['ESTADO'];
-    $fechaTotal = $fila['FECHA_TOTAL'];
 
-    if (!isset($grupos[$nombreZona])) {
-        $grupos[$nombreZona] = array();
-    }
+// $grupos = array();
 
-    $grupos[$nombreZona][] = array(
-        'nombreInfraestructura' => $nombreInfraestructura,
-        'estado' => $estado,
-        'fechaTotal' => $fechaTotal
-    );
-}
+// foreach ($datos as $fila) {
+//     $nombreZona = $fila['NOMBRE_T_ZONA_AREAS'];
+//     $nombreInfraestructura = $fila['NOMBRE_INFRAESTRUCTURA'];
+//     $estado = $fila['ESTADO'];
+//     $fechaTotal = $fila['FECHA_TOTAL'];
+
+//     if (!isset($grupos[$nombreZona])) {
+//         $grupos[$nombreZona] = array();
+//     }
+
+//     $grupos[$nombreZona][] = array(
+//         'nombreInfraestructura' => $nombreInfraestructura,
+//         'estado' => $estado,
+//         'fechaTotal' => $fechaTotal
+//     );
+// }
+
 
 // foreach ($grupos as $nombreZona => $valores) {
 //     $html .= '<tr>';
@@ -87,9 +90,17 @@ foreach ($datos as $fila) {
 //     // Obtener el último día del mes
 //     $ultimoDiaMes = (new DateTime($anio . '-' . $mes . '-01'))->format('t');
 
+//     // Calcular el número total de columnas
+//     $numColumnas = 3 + $ultimoDiaMes;
+
 //     // Imprimir columnas adicionales a partir de la columna 4
-//     for ($i = 4; $i <= ($ultimoDiaMes + 3); $i++) {
+//     for ($i = 4; $i <= $numColumnas; $i++) {
 //         $html .= '<td>' . ($i - 3) . '</td>';
+//     }
+
+//     // Completar las columnas faltantes hasta 31 días
+//     for ($i = $numColumnas + 1; $i <= 34; $i++) {
+//         $html .= '<td></td>';
 //     }
 
 //     $html .= '</tr>';
@@ -107,16 +118,47 @@ foreach ($datos as $fila) {
 //         // Obtener el último día del mes
 //         $ultimoDiaMes = (new DateTime($anio . '-' . $mes . '-01'))->format('t');
 
+//         // Calcular el número total de columnas
+//         $numColumnas = 3 + $ultimoDiaMes;
+
 //         // Imprimir columnas adicionales a partir de la columna 4
-//         for ($k = 4; $k <= ($ultimoDiaMes + 3); $k++) {
+//         for ($k = 4; $k <= $numColumnas; $k++) {
 //             $html .= '<td>' . ($k - 3) . '</td>';
 //         }
 
-
+//         // Completar las columnas faltantes hasta 31 días
+//         for ($k = $numColumnas + 1; $k <= 34; $k++) {
+//             $html .= '<td></td>';
+//         }
 
 //         $html .= '</tr>';
 //     }
 // }
+
+
+
+$grupos = array();
+
+foreach ($datos as $fila) {
+    $nombreZona = $fila['NOMBRE_T_ZONA_AREAS'];
+    $nombreInfraestructura = $fila['NOMBRE_INFRAESTRUCTURA'];
+    $estado = $fila['ESTADO'];
+    $fechaTotal = $fila['FECHA_TOTAL'];
+
+    if (!isset($grupos[$nombreZona])) {
+        $grupos[$nombreZona] = array();
+    }
+
+    // Agregar la condición para omitir los estados 'P'
+    if ($estado != 'P') {
+        $grupos[$nombreZona][] = array(
+            'nombreInfraestructura' => $nombreInfraestructura,
+            'estado' => $estado,
+            'fechaTotal' => $fechaTotal
+        );
+    }
+}
+
 foreach ($grupos as $nombreZona => $valores) {
     $html .= '<tr>';
     $html .= '<td rowspan="' . count($valores) . '">' . $nombreZona . '</td>';
@@ -175,6 +217,7 @@ foreach ($grupos as $nombreZona => $valores) {
         $html .= '</tr>';
     }
 }
+
 
 
 
