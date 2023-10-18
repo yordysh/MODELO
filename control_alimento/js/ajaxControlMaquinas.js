3004d7d406e654a1f8b920fe0750ab1280de47e9
yordysh <yordysh28@gmail.com>
2023-10-18 14:27:08 -0500

    modified

--- control_alimento/js/ajaxControlMaquinas.js
+++ control_alimento/js/ajaxControlMaquinas.js
@@ -3,31 +3,31 @@ $(function () {
   let edit = false;
 
   //------------- MENU BAR JS ---------------//
-  let nav = document.querySelector(".nav"),
-    searchIcon = document.querySelector("#searchIcon"),
-    navOpenBtn = document.querySelector(".navOpenBtn"),
-    navCloseBtn = document.querySelector(".navCloseBtn");
+  // let nav = document.querySelector(".nav"),
+  //   searchIcon = document.querySelector("#searchIcon"),
+  //   navOpenBtn = document.querySelector(".navOpenBtn"),
+  //   navCloseBtn = document.querySelector(".navCloseBtn");
 
-  searchIcon.addEventListener("click", () => {
-    nav.classList.toggle("openSearch");
-    nav.classList.remove("openNav");
-    if (nav.classList.contains("openSearch")) {
-      return searchIcon.classList.replace(
-        "icon-magnifying-glass",
-        "icon-cross"
-      );
-    }
-    searchIcon.classList.replace("icon-cross", "icon-magnifying-glass");
-  });
+  // searchIcon.addEventListener("click", () => {
+  //   nav.classList.toggle("openSearch");
+  //   nav.classList.remove("openNav");
+  //   if (nav.classList.contains("openSearch")) {
+  //     return searchIcon.classList.replace(
+  //       "icon-magnifying-glass",
+  //       "icon-cross"
+  //     );
+  //   }
+  //   searchIcon.classList.replace("icon-cross", "icon-magnifying-glass");
+  // });
 
-  navOpenBtn.addEventListener("click", () => {
-    nav.classList.add("openNav");
-    nav.classList.remove("openSearch");
-  });
+  // navOpenBtn.addEventListener("click", () => {
+  //   nav.classList.add("openNav");
+  //   nav.classList.remove("openSearch");
+  // });
 
-  navCloseBtn.addEventListener("click", () => {
-    nav.classList.remove("openNav");
-  });
+  // navCloseBtn.addEventListener("click", () => {
+  //   nav.classList.remove("openNav");
+  // });
 
   //----------------------------------------------------------------//
 
